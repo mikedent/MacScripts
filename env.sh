fc8544eb3034f7a27c0cb3b470d4ba6107994448
Mike Dent <mike.dent@gmail.com>
2016-10-23 14:58:39 -0400

    Adding env.sh script for .zshrc sourcing

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# PATH
+export PATH="/usr/local/share/python:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export EDITOR='code'
+
+# Use VS Code for editing config files
+alias zshconfig="code ~/.zshrc"
+alias envconfig="code ~/env.sh"
